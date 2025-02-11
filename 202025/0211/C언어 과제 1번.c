6f976f80d22eeefee9a1d869725cda590d3acb39
Beom <113332125+BeomKung@users.noreply.github.com>
2025-02-11 16:06:13 +0900

    Update C언어 과제 1번.c

--- 202025/0211/C언어 과제 1번.c
+++ 202025/0211/C언어 과제 1번.c
@@ -4,6 +4,7 @@ scanf("%d %d %d", &a, &b, &c);
 if (c == 0) {
     printf("0으로 나누기 ㄴㄴ\n");
 } else {
-    int result = (a + b) * c - (a - b) / c;
-    printf("%d\n", result);
+    double result = (a + b) * c - (double)(a - b) / c;
+    printf("%.2f\n", result);
+}
 }
