6c60c51002d7a857dd0388f4f1c45064da03072d
Beom <113332125+BeomKung@users.noreply.github.com>
2025-02-18 17:20:10 +0900

    Create 차량 종류를 열거형으로 정의하고 선택하여 출력하기.c

---
+++ 202025/0219/차량 종류를 열거형으로 정의하고 선택하여 출력하기.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{ 
+  enum cartype {
+    SEDAN = 1,
+    SUV = 2,
+    TRUCK = 3,
+    VAN = 4  
+    };
+    
+    char *sedan[] = {"AVANTE", "SONATA", "MALIBU", "K5", "CAMRY", "GRANDEUR"};
+    char *suv[] = {"TUCSON", "SPORTAGE", "SANTAFE", "KONA", "RAV4"};
+    char *truck[] = {"RAM", "FORD", "PORTER", "MIGHTY", "BONGO"};
+    char *van[] = {"SIENNA", "CARNIVAL", "PACIFICA", "ODYSSEY"};
+
+    char choice[50];
+    enum cartype carType;
+    
+    printf("차량 모델를 입력하세요 : ");
+    scanf("%s", choice);
+    
+    for (int i = 0; i < sizeof(sedan) / sizeof(sedan[0]); i++){
+        if (strcmp(choice, sedan[i]) == 0) {
+            carType = SEDAN;
+            break;
+        }
+    }
+    for (int i = 0; i < sizeof(suv) / sizeof(suv[0]); i++){
+        if (strcmp(choice, suv[i]) == 0) {
+            carType = SUV;
+            break;
+        }
+    }
+    for (int i = 0; i < sizeof(truck) / sizeof(truck[0]); i++){
+        if (strcmp(choice, truck[i]) == 0) {
+            carType = TRUCK;
+            break;
+            
+        }
+    }
+    for (int i = 0; i < sizeof(van) / sizeof(van[0]); i++){
+        if (strcmp(choice, van[i]) == 0) {
+            carType = VAN;
+            break;
+        }
+    }
+    
+    printf("현재 선택된 종류 %d \n", carType);
+    
+    switch (carType) {
+        case SEDAN:
+            printf("선택한 차량 종류는: SEDAN\n");
+            break;
+        case SUV:
+            printf("선택한 차량 종류는: SUV\n");
+            break;
+        case TRUCK:
+            printf("선택한 차량 종류는: TRUCK\n");
+            break;
+        case VAN:
+            printf("선택한 차량 종류는: VAN\n");
+            break;
+        default:
+            printf("잘못된 선택입니다.\n");
+            break;
+    }
+    
+}
