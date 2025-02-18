fcb989b10be308aeb3bcc2d69e5d51a806ee1069
Beom <113332125+BeomKung@users.noreply.github.com>
2025-02-18 17:21:06 +0900

    Create 다양한 데이터 타입을 저장하는 공용체 만들기.c

---
+++ 202025/0219/다양한 데이터 타입을 저장하는 공용체 만들기.c
@@ -0,0 +1,15 @@
+union num {
+        int intVal;
+        float floatVal;
+        char charVal;
+    };
+    
+    union num n;
+    n.floatVal = 3.14;
+    n.intVal = 100;
+    n.charVal = 'C';
+    
+    printf("%f %d\n", n.floatVal, &n.floatVal);
+    printf("%d %d\n", n.intVal, &n.intVal);
+    printf("%c %d\n", n.charVal, &n.charVal);
+    
